df92b75624de2926ff08513be6121140e2c8962b
Gokul-47 <gokul7010645@gmail.com>
2025-10-08 15:05:36 +0530

    Update Home1.html and other changes

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true
+  });
 
+  document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add("loaded");
+  });
+
+  
+  document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      const href = this.getAttribute('href');
+
+   
+      if (href && !href.startsWith('http') && href !== '#') {
+        e.preventDefault();
+
+        document.body.classList.remove('loaded'); 
+
+        setTimeout(() => {
+          window.location.href = href;
+        }, 600); 
+      }
+    });
+  });
